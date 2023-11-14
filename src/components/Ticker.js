e155ddc3bdd0f5100627c15e50636189bc147da1
Isaac <govegeku00@gmail.com>
2023-11-14 22:40:33 +0300

    Box changes

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,16 +1,35 @@
-import React, { useEffect, useState } from "react";
+//Implements the first feature of generating a random price each second
+
+import React, { useEffect, useRef, useState } from "react";
 import { makeRandomNumber } from "../utils";
 
 function Ticker() {
   const [price, setPrice] = useState(0);
   const [color, setColor] = useState("black");
+  //create the ref and set its initial value
+  const prevPriceRef = useRef(price)
 
   useEffect(() => {
+    //every 1s, generate a new random price
     const id = setInterval(() => setPrice(makeRandomNumber), 1000);
     return function () {
       clearInterval(id);
     };
   }, []);
+
+  useEffect(() => {
+    //use the current value of the ref
+    const prevPrice = prevPriceRef.current
+    if (price > prevPrice) {
+      setColor("green")
+    } else if (price < prevPrice) {
+      setColor("red")
+    } else {
+      setColor("black")
+    }
+    //set the new value of the ref
+    prevPriceRef.current = price
+  }, [price])
 
   return (
     <div>
